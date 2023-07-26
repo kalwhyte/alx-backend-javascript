fec0f8f2c012122e1d1e9bb34695786869a63a61
kalwhyte <superkalel55@gmail.com>
2023-07-27 00:38:26 +0100

    To implement the handleProfileSignup function, you can use Promise.all() to collectively resolve multiple promises from uploadPhoto and createUser functions. If any of the promises get rejected, you can handle the error using .catch() and log the error message to the console.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,12 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      console.log(
-        `${results[0].body} ${results[1].firstName} ${results[1].lastName}`,
-      );
-    })
-    .catch(() => console.log('Signup system offline'));
+export default async function handleProfileSignup() {
+  try {
+    const results = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(
+      `${results[0].body} ${results[1].firstName} ${results[1].lastName}`,
+    );
+  } catch {
+    return console.log('Signup system offline');
+  }
 }
