5f0bff5b05d6b5732b143fc76bfe6c543b792557
kalwhyte <superkalel55@gmail.com>
2023-07-27 01:30:56 +0100

    make it a true story

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,8 @@
 export default function getFullResponseFromAPI() {
   return new Promise((resolve, reject) => {
     if (true) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    }
-    if (false) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
