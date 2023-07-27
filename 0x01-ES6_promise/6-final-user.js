cae07faa523c599d51cf8b077ac28bf3332caae5
kalwhyte <superkalel55@gmail.com>
2023-07-27 01:58:52 +0100

    we use Promise.allSettled() to handle both promises returned by signUpUser and uploadPhoto. The Promise.allSettled() method returns a promise that resolves after all of the given promises have either fulfilled or rejected, and it provides an array of objects with the status and the value or reason for each promise.

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,33 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((res) =>
-    res.map((o) => ({
-      status: o.status,
-      value: o.status === 'fulfilled' ? o.value : String(o.reason),
-    })),
-  );
+async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const [userResponse, photoResponse] = await Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]);
+
+    return [
+      {
+        status: userResponse.status,
+        value:
+          userResponse.status === 'fulfilled'
+            ? userResponse.value
+            : userResponse.reason,
+      },
+      {
+        status: photoResponse.status,
+        value:
+          photoResponse.status === 'fulfilled'
+            ? photoResponse.value
+            : photoResponse.reason,
+      },
+    ];
+  } catch (err) {
+    console.log('Error in handleProfileSignup:', err);
+    return [];
+  }
 }
+
+export default handleProfileSignup;
