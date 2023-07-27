e38a7b9598a3a897602499377d42b0fad8a6067f
kalwhyte <superkalel55@gmail.com>
2023-07-27 01:41:38 +0100

    To implement the handleResponseFromAPI function, you can use the .then() method to add a fulfillment handler when the promise resolves and the .catch() method to add a rejection handler when the promise rejects.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,13 @@
-export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => new Error())
-    .finally(() => {
+function handleResponseFromAPI(promise) {
+  promise
+    .then((response) => {
       console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch((error) => {
+      console.log('Got a response from the API');
+      return new Error();
     });
 }
+
+export default handleResponseFromAPI;
