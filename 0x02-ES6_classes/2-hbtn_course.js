0ce80b4d3380cc19433e5a9dac5668cfa5c37a0d
kalwhyte <superkalel55@gmail.com>
2023-07-29 03:02:11 +0100

    HolbertonCourse class will properly check the types of its constructor arguments and throw the respective TypeError if they are not as expected. If everything is correct, it will initialize the _name, _length, and _students properties accordingly.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,19 +1,22 @@
+/* eslint-disable operator-linebreak */
 /* eslint-disable no-underscore-dangle */
-export default function HolbertonCourse(name, length, students) {
-  if (typeof name !== 'string') {
-    throw TypeError('Name must be a string');
-  }
-  if (typeof length !== 'number') {
-    throw TypeError('Length must be a number');
-  }
-  if (!Array.isArray(students)) {
-    throw TypeError('Students must be an array of strings');
-  }
-  if (students.some((student) => typeof student !== 'string')) {
-    throw TypeError('Students must be an array of strings');
-  }
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (
+      !Array.isArray(students) ||
+      students.some((student) => typeof student !== 'string')
+    ) {
+      throw TypeError('Students must be an array of strings');
+    }
 
-  this._name = name;
-  this._length = length;
-  this._students = students;
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 }
