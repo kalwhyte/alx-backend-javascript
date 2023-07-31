e34134a90ed986c6ef80813ea185a205a60a2d25
kalwhyte <superkalel55@gmail.com>
2023-07-31 18:20:55 +0100

    modified the implementation of filter

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,22 +1,15 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  if (Array.isArray(students) === false) {
-    return [];
-  }
-  const studentsByCity = students.filter(
-    (student) => student.location === city,
-  );
-  const studentIds = studentsByCity.map((student) => student.id);
-  const updatedStudents = studentsByCity.map((student) => {
-    const grade = newGrades.filter(
-      (newGrade) => newGrade.studentId === student.id,
-    );
-    if (grade.length === 0) {
-      return { ...student, grade: 'N/A' };
-    }
-    return { ...student, grade: grade[0].grade };
-  });
-  const otherStudents = students.filter(
-    (student) => studentIds.indexOf(student.id) === -1,
-  );
-  return [...updatedStudents, ...otherStudents];
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrades.find((grade) => grade.studentId === student.id);
+      if (grade) {
+        return { ...student, grade: grade.grade };
+        // eslint-disable-next-line no-else-return
+      } else {
+        return { ...student, grade: 'N/A' };
+      }
+    });
 }
+
+export default updateStudentGradeByCity;
