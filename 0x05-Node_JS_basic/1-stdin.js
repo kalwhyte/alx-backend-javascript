1759f9365e66357f4d03a9042eb258d5f57fdc32
Whytewalker <whytewalkerinstallation@gmail.com>
2023-09-18 19:45:04 +0100

    a program named 1-stdin.js that will be executed through command line

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+// Create a program named 1-stdin.js
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Welcome to Holberton School, what is your name? ', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
