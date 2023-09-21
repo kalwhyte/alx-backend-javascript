680fc527f0b118f04424f86741ead3aed7c138db
kalwhyte <superkalel55@gmail.com>
2023-09-21 11:29:49 +0100

    Update

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,21 +4,21 @@ const fs = require('fs');
  * Counting students.
  * @param {string} path.
  */
-const countStudents = (path) =>
+const countStudents = (dataPath) =>
   new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, whyte) => {
+    fs.readFile(dataPath, 'utf-8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
       }
-      if (whyte) {
-        const fileLines = whyte.toString('utf-8').trim().split('\n');
+      if (data) {
+        const fileLines = data.toString('utf-8').trim().split('\n');
         const studentGroups = {};
-        const dbFieldNames = fileLines.shift().split(',');
+        const dbFieldNames = fileLines[0].split(',');
         const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
         for (const line of fileLines.slice(1)) {
           const studentRecord = line.split(',');
-          const studentPropValue = studentRecord.slice(
+          const studentPropValues = studentRecord.slice(
             0,
             studentRecord.length - 1,
           );
@@ -28,7 +28,7 @@ const countStudents = (path) =>
           }
           const studentEntries = studentPropNames.map((propName, idx) => [
             propName,
-            studentPropValue[idx],
+            studentPropValues[idx],
           ]);
           studentGroups[field].push(Object.fromEntries(studentEntries));
         }
